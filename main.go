143e98edd8e4c8782cda672b07fd56f1deda3afa
Apple <apple@Alireza-MacBook-Pro.local>
2023-12-01 23:57:00 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+
+	"jwt-auth-api/app"
+	"jwt-auth-api/controllers"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+    router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
